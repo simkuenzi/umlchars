8cdc9a03f0800f995a3d43b63043e4aabc2ff642
Simon Künzi <sim.kuenzi@gmail.com>
2020-06-28 09:01:35 +0200

    Explicit charset in content-type header

--- src/main/java/com/github/simkuenzi/umlchars/Controller.java
+++ src/main/java/com/github/simkuenzi/umlchars/Controller.java
@@ -7,11 +7,11 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -31,18 +31,13 @@ public class Controller {
         this.templateEngine = templateEngine;
     }
 
-    String render(String template) throws Exception {
-        return render(template, new HashMap<>());
-    }
-
-    protected String render(String template, Map<String, Object> vars) throws Exception {
+    protected Response render(String template, Map<String, Object> vars) throws Exception {
         Properties versionProps = new Properties();
         versionProps.load(Controller.class.getResourceAsStream("version.properties"));
         vars.put("version", versionProps.getProperty("version"));
         WebContext context = new WebContext(servletRequest, servletResponse, servletContext, servletRequest.getLocale(), vars);
-        return templateEngine.process(template, context);
+        return Response.ok(templateEngine.process(template, context), MediaType.TEXT_HTML_TYPE.withCharset("utf-8")).build();
     }
-
 
     protected Response redirect(String path) throws URISyntaxException {
         return Response.seeOther(new URI(uriInfo.getBaseUri() + path)).build();
--- src/main/java/com/github/simkuenzi/umlchars/Home.java
+++ src/main/java/com/github/simkuenzi/umlchars/Home.java
@@ -1,17 +1,14 @@
 package com.github.simkuenzi.umlchars;
 
-import com.github.simkuenzi.restforms.FormField;
 import org.thymeleaf.TemplateEngine;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Path("")
 public class Home extends Controller {
@@ -21,7 +18,7 @@ public class Home extends Controller {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public String get() throws Exception {
+    public Response get() throws Exception {
         Map<String, Object> vars = new HashMap<>();
         vars.put("form", new UmlForm().noValidation());
         return render("home", vars);
@@ -41,6 +38,6 @@ public class Home extends Controller {
                     .asText()
             );
         }
-        return Response.ok(render("home", vars), MediaType.TEXT_HTML).build();
+        return render("home", vars);
     }
 }
